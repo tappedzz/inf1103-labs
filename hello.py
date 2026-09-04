1a2e03e05691bdba547a49f06c8729ab91490638
tapped <ianwyz000@gmail.com>
2026-09-04 17:06:19 +0800

    Week 1 Activity 4

--- hello.py
+++ hello.py
@@ -19,3 +19,13 @@ print('Day 3 Followers:', followers)
 print('Username:', username)
 print('Bio:', bio)
 print('Followers:', followers)
+
+username=input('Enter username: ')
+age=input('Enter Age: ')
+category=input('Enter Content Category: ')
+
+print("\nInstagram Profile")
+print("=======================")
+print('Username:', username)
+print('Age:', age)
+print('Category:', category)
